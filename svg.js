9374388668a2d8566c86716b8ff8276f9ad9a12a
ahmetulutas <englishlectures.stocks@gmail.com>
2022-02-18 23:35:15 +0300

    week2 project final

---
+++ svg.js
@@ -0,0 +1,11 @@
+export const svgFiles = {
+    success:`<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+    <circle style="fill:#25AE88;" cx="25" cy="25" r="25"/>
+    <polyline style="fill:none;stroke:#FFFFFF;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;" points="  38,15 22,33 12,25 "/>
+    </svg>`,
+    fail:`<svg xmlns="http://www.w3.org/2000/svg" width="25px" height="25px" viewBox="0 0 12 12">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M6 12A6 6 0 106 0a6 6 0 000 12zM3 5a1 1 0 000 2h6a1 1 0 100-2H3z" fill="red"/>
+    </svg>`
+}
+
+
